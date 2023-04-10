1e20fd4eeff9cbcf1b1bb59fdf93a4ad949c430c
Lukyn Průša <lukyn.patai5@gmail.com>
2023-04-10 19:11:57 +0200

    Centered the chessboard on the screen.

--- frontend/src/components/Play/Play.tsx
+++ frontend/src/components/Play/Play.tsx
@@ -4,9 +4,20 @@ import React from "react";
 import Chessboard from "./ChessBoard/ChessBoard";
 import { Color } from "./ChessBoard/ChessLogic/pieces";
 
-type Props = { color: Color };
+const playCss = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+type Props = { color?: Color };
 export default function Play(props: Props) {
     const { color = Color.White } = props;
 
-    return <Chessboard color={color} />;
+    return (
+        <div css={playCss}>
+            <Chessboard color={color} />
+        </div>
+    );
 }
