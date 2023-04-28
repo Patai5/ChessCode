b0255d6d9e127d13465a859ecd2164fa5bd826fe
Lukyn Průša <lukyn.patai5@gmail.com>
2023-04-28 23:24:37 +0200

    Updated game and move database models.

--- backend/api/play/models.py
+++ backend/api/play/models.py
@@ -1,40 +1,43 @@
-import chess
 from django.contrib.auth import get_user_model
 from django.db import models
-
-from .chess_board import CustomTermination
 
 User = get_user_model()
 
 
-class Move(models.Model):
-    models.UniqueConstraint(name="unique_key", fields=["game", "order"])
-    game = models.ForeignKey(
-        "Game",
-        on_delete=models.CASCADE,
-    )
-    order = models.IntegerField()
-    move = models.CharField(max_length=5)
-
-
 class GameTerminations(models.IntegerChoices):
-    0, chess.Termination.CHECKMATE,
-    1, chess.Termination.STALEMATE,
-    2, chess.Termination.INSUFFICIENT_MATERIAL,
-    3, chess.Termination.FIFTY_MOVES,
-    4, chess.Termination.THREEFOLD_REPETITION,
-    5, CustomTermination.TIMEOUT,
-    6, CustomTermination.RESIGNATION,
-    7, CustomTermination.AGREEMENT,
+    CHECKMATE = 0, "CHECKMATE"
+    STALEMATE = 1, "STALEMATE"
+    INSUFFICIENT_MATERIAL = 2, "INSUFFICIENT_MATERIAL"
+    FIFTY_MOVES = 3, "FIFTY_MOVES"
+    THREEFOLD_REPETITION = 4, "THREEFOLD_REPETITION"
+    TIMEOUT = 5, "TIMEOUT"
+    RESIGNATION = 6, "RESIGNATION"
+    AGREEMENT = 7, "AGREEMENT"
 
 
 class Game(models.Model):
-    game_id = models.CharField(max_length=8, unique=True)
+    game_id = models.AutoField(primary_key=True)
     player_white = models.ForeignKey(User, related_name="player_white", on_delete=models.SET_NULL, null=True)
     player_black = models.ForeignKey(User, related_name="player_black", on_delete=models.SET_NULL, null=True)
-    termination = models.IntegerField(choices=GameTerminations)
+    termination = models.IntegerField(choices=GameTerminations.choices)
+    winner_color = models.BooleanField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        get_latest_by = ["date"]
+
+    def __str__(self):
+        return str(self.game_id)
 
 
-class Game_winner(models.Model):
+class Move(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    winner = models.BooleanField()
+    order = models.PositiveIntegerField()
+    move = models.CharField(max_length=5)
+
+    class Meta:
+        get_latest_by = ("game", "order")
+        unique_together = ("game", "order")
+
+    def __str__(self):
+        return f"{self.game} - {self.order} - {self.move}"
