30980197dcb41d76d1f84c388ee08e6c067b01c5
Lukyn Průša <lukyn.patai5@gmail.com>
2023-05-28 00:08:14 +0200

    Forgot to commit this file in commit #8f37b97.

--- frontend/src/components/Play/Chess/ActionBar/Player/Player.tsx
+++ frontend/src/components/Play/Chess/ActionBar/Player/Player.tsx
@@ -1,34 +1,46 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import Paper from "components/shared/Paper";
+import Dropdown, { DropdownItems } from "components/shared/Dropdown/Dropdown";
 import ProfilePicture from "components/shared/ProfilePicture";
+import { FaUser } from "react-icons/fa";
 
-const PlayerContainer = css`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 0.5em;
-
+const PlayerCss = css`
     margin-right: auto;
+    border-radius: 0.35em;
 `;
 const IconCss = css`
     width: 1.75em;
     height: 1.75em;
 `;
 const UsernameCss = css`
-    color: white;
-    margin: 0;
-    font-family: "Lexend Deca", sans-serif;
+    max-width: 10em;
     font-weight: 500;
     font-size: 0.8em;
+    overflow: hidden;
+    text-overflow: ellipsis;
 `;
-
-type Props = { username: string };
+const UsernameContainerCss = css`
+    padding: 0.5em;
+`;
+const DropdownCss = css`
+    left: 50%;
+    translate: -50% 0;
+    box-shadow: 0 0 0.5em 0.2em #000000ab, inset 0 0 0.3em #000000ab;
+`;
+type Props = { username: string; isOpponent: boolean };
 export default function Player(props: Props) {
-    return (
-        <Paper customCss={PlayerContainer} elevation={1} white={true}>
-            <ProfilePicture customCss={IconCss} username={props.username} />
-            <p css={UsernameCss}>{props.username}</p>
-        </Paper>
-    );
+    const dropdownItems: DropdownItems = {
+        main: {
+            image: <ProfilePicture username={props.username} customCss={IconCss} />,
+            text: props.username,
+            onClick: () => {},
+            textCss: UsernameCss,
+            customCss: UsernameContainerCss,
+        },
+        items: [{ icon: FaUser, text: "Profile", onClick: () => {} }],
+        dropdownCss: DropdownCss,
+    };
+    if (props.isOpponent) dropdownItems.items.push({ icon: FaUser, text: "Friend", onClick: () => {} });
+
+    return <Dropdown dropdownItems={dropdownItems} customCss={PlayerCss} upwards={!props.isOpponent} />;
 }
