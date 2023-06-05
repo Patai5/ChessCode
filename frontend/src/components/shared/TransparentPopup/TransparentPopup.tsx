c86fc5d8174db61378252bc0568f89b029852ee3
Lukyn Průša <lukyn.patai5@gmail.com>
2023-06-05 21:32:03 +0200

    Improved method for centering popups with blur on screen.

--- frontend/src/components/shared/TransparentPopup/TransparentPopup.tsx
+++ frontend/src/components/shared/TransparentPopup/TransparentPopup.tsx
@@ -10,31 +10,43 @@ const PopupCss = css`
     background: rgba(36, 36, 36, 0.97);
     backdrop-filter: blur(0.1em);
     border-radius: 0.5em;
-
+    pointer-events: auto;
     z-index: 1;
-    position: absolute;
-    top: 50%;
-    left: 50%;
 
     transition: transform ${animationLength}ms ease-in-out;
     transition-property: opacity, transform;
     opacity: 0;
-    transform: translate(-50%, -50%) scale(0);
+    transform: scale(0);
 `;
 const OpenAnimationCss = css`
     opacity: 1;
-    transform: translate(-50%, -50%) scale(1);
+    transform: scale(1);
 `;
 const ClosedAnimationCss = css`
     opacity: 0;
-    transform: translate(-50%, -50%) scale(0);
+    transform: scale(0);
+`;
+
+const CenteringContainerCss = css`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 type Props = { show: boolean; children: React.ReactNode; customCss?: SerializedStyles };
 export default function TransparentPopup(props: Props) {
     return (
-        <div css={[PopupCss, props.show === true ? OpenAnimationCss : ClosedAnimationCss, props.customCss]}>
-            {props.children}
+        <div css={CenteringContainerCss}>
+            <div css={[PopupCss, props.show === true ? OpenAnimationCss : ClosedAnimationCss, props.customCss]}>
+                {props.children}
+            </div>
         </div>
     );
 }
