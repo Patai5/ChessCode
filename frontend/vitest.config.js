27e5505aa40c1f51ddb4644d4e8b4646bd4e8021
Patai5 <lukyn.patai5@gmail.com>
2024-08-18 21:41:39 +0200

    test(unit): add replay system tests

---
+++ frontend/vitest.config.js
@@ -0,0 +1,13 @@
+import path from "path";
+
+export default {
+    test: { environment: "jsdom", globals: true },
+    resolve: {
+        alias: {
+            components: path.resolve(__dirname, "./src/components/"),
+            utils: path.resolve(__dirname, "./src/utils/"),
+            hooks: path.resolve(__dirname, "./src/hooks/"),
+            types: path.resolve(__dirname, "./src/types/"),
+        },
+    },
+};
