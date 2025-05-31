fb6577b06a0eaa6f3d1bf650f667ac2679f4b657
Patai5 <lukyn.patai5@gmail.com>
2025-05-31 16:43:15 +0200

    fix: update deprecated request type

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from rest_framework.request import Request
+from django.http import HttpRequest
 
 
 class Main(View):
-    def get(self, request: Request) -> HttpResponse:
+    def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, "frontend/index.html", {})
